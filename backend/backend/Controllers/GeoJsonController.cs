5650ad7189e29e172567572501c78acad34a18a6
Akash1590 <ar156403@gmail.com>
2023-11-28 01:41:35 +0100

    first commit

---
+++ backend/backend/Controllers/GeoJsonController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GeoJSON.Net.Feature;
+using Newtonsoft.Json;
+using backend.Models;
+using Microsoft.Net.Http.Headers;
+
+namespace backend.Controllers
+{
+    
+    [Route("api/[controller]")]
+	[ApiController]
+	public class GeoJsonController : ControllerBase
+	{
+        private readonly string filePath = "Data/telekom_bs_bonn.geojson";
+        [HttpGet]
+		public IActionResult GetGeoJsonData()
+		{
+            
+			string json = System.IO.File.ReadAllText(filePath);
+			var featureCollection = JsonConvert.DeserializeObject<GeoJsonFeatureCollection>(json);
+			var geoJsonFeatures = featureCollection.Features;
+            /*foreach (var feature in geoJsonFeatures)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(feature));
+            }*/
+            DateTime currentTime = DateTime.Now;
+
+            // Format the current time as a string
+            string formattedTime = currentTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            // Print the message along with the current time
+            Console.WriteLine($"Fetched at {formattedTime}");
+            /*Console.WriteLine("Fetched");*/
+            //var featureCollection = JsonConvert.DeserializeObject<FeatureCollection<GeoJsonFeature>>(json);
+            Response.ContentType = "application/vnd.geo+json";
+            /*var contentDispositionHeader = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "telekom_bs_bonn.geojson"
+            };
+            Response.Headers.Add(HeaderNames.ContentDisposition, contentDispositionHeader.ToString());*/
+
+            return Ok(geoJsonFeatures);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetGeoJsonFeatureById(int id)
+        {
+            string json = System.IO.File.ReadAllText(filePath);
+            var featureCollection = JsonConvert.DeserializeObject<GeoJsonFeatureCollection>(json);
+            // Find the feature with the specified ID
+            var geoJsonFeature = featureCollection.Features.Find(feature => feature.Id == id);
+            Response.ContentType = "application/vnd.geo+json";
+
+            if (geoJsonFeature == null)
+            {
+                return Ok(new { });
+            }
+
+            return Ok(geoJsonFeature);
+
+
+        }
+
+    }
+}
---
+++ backend/backend/Models/GeoJsonModels.cs
@@ -0,0 +1,108 @@
+﻿/*using System.Collections.Generic;
+using System.IO;
+using backend.Models;
+using GeoJSON.Net.Feature;
+using Newtonsoft.Json;
+
+namespace backend.Models
+{
+    
+
+    public class GeoJsonFeature
+	{
+		public int Id { get; set; }
+		public string Type { get; set; }
+		public Geometry Geometry { get; set; }
+		public Properties Properties { get; set; }
+		public Style Style { get; set; }
+
+	}
+	public class Geometry
+	{
+		public string Type { get; set; }
+		public List<double> Coordinates { get; set; }
+	}
+
+    public class Properties
+    {
+        public string Name { get; set; }
+        public int Mcc { get; set; }
+        public int Mnc { get; set; }
+        public int Area { get; set; }
+        public long CellId { get; set; }
+        public string SysSubtype { get; set; }
+        public string PCI { get; set; }
+        public string Bandwidth { get; set; }
+        public int EARFCN { get; set; }
+        public string MaxRSRP { get; set; }
+        public string Direction { get; set; }
+        public string Height { get; set; }
+        public string UplinkFreq { get; set; }
+        public string DownlinkFreq { get; set; }
+        public string FreqBand { get; set; }
+        public string Time { get; set; }
+    }
+    public class Style
+    {
+        public string StrokeWidth { get; set; }
+        public double FillOpacity { get; set; }
+    }
+
+
+}
+*/
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace backend.Models
+{
+    public class GeoJsonFeatureCollection
+    {
+        public List<GeoJsonFeature> Features { get; set; }
+    }
+
+    public class GeoJsonFeature
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+        public Style Style { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public List<double> Coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Name { get; set; }
+        public int Mcc { get; set; }
+        public int Mnc { get; set; }
+        public int Area { get; set; }
+        public long Cell_Id { get; set; }
+        public string Sys_Subtype { get; set; }
+        public string PCI { get; set; }
+        public string Bandwidth { get; set; }
+        public int EARFCN { get; set; }
+        public string Max_RSRP { get; set; }
+        public string Direction { get; set; }
+        public string Height { get; set; }
+        public string Uplink_Freq { get; set; }
+        public string Downlink_Freq { get; set; }
+        public string Freq_Band { get; set; }
+        public string Time { get; set; }
+    }
+
+    public class Style
+    {
+        [JsonProperty("stroke-width")] // This attribute is used to map the property to the JSON key
+        public string StrokeWidth { get; set; }
+
+        [JsonProperty("fill-opacity")]
+        public double FillOpacity { get; set; }
+    }
+}
